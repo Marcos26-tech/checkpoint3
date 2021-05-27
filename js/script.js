a9841291350458431f0ecbd2cc0878a081c7653e
Marcos26-tech <rm88267@fiap.com.br>
2021-05-27 19:25:48 -0300

    ajuste no js

--- js/script.js
+++ js/script.js
@@ -1,52 +1,75 @@
 //scrpt para verificar tds os seletores//
-const fields = document.querySelectorAll("[required]")
+const fields = document.querySelectorAll("[input]")
 
 console.log(fields)
 
 function customValidation(event) {
     const field = event.target
 
-    function verifyErrors(){
+    function verifyErrors() {
         let foundError = false;
-        for(error in field.validity){
-
+        for (let error in field.validity) {
+            if (error != "customError" && field.validity[error]) {
+                foundError = error
+            }
         }
         return foundError;
     }
-    verifyErrors()
+    const error = verifyErrors()
+    console.log("Error Existem ", error)
 
-    //troca a msg no required//
-    field.setCustomValidity("Esse campo é obrigatório")
+    if (erro) { //troca a msg no required//
+        field.setCustomValidity("Esse campo é obrigatório")
+    } else {
+        field.setCustomValidity("")
+    }
+
+    for (let field of fields) {
+        field.addEventListener("invalid", customValidation)
+    }
+
+
+
+    let arquivoEmails = ["marcosmaciel@fiap.com.br", "ma@fiap.com.br", "marcos@fiap.com.br"];
+    let senhas = [" rm88267", "rm822665", "rm52565"];
+
+    function login() {
+        var username = document.login.Email.value;
+        username = Email.toLowerCase();
+        var password = document.login.pass.value;
+        password = pass.toLowerCase();
+        for (var cont = 0; cont < arquivoEmails.length; cont++)
+            if (username == "arquivoEmails[cont]" && password == "senhas[cont]") {
+                window.location = "../portal.html";
+                done = 1;
+
+            }
+    }
 
 }
 
-for(let field of fields){
-    field.addEventListener("invalid", customValidation)
+var arrayPessoas = []
+if (JSON.parse(localStorage.getItem("pessoas")) != null) {
+    arrayPessoas.push(JSON.parse(localStorage.getItem("pessoas")));
 }
+var nome = document.getElementById("inputNome").value;
+var cpf = document.getElementById("inputCPF").value;
+var idade = document.getElementById("inputIdade").value;
+var telefone = document.getElementById("inputTelefone").value;
+var email = document.getElementById("inputEmail").value;
+var senha = document.getElementById("inputSenha").value;
 
+var pessoa = {
+    nome: nome,
+    cpf: cpf,
+    idade: idade,
+    telefone: telefone,
+    email: email,
+    senha: senha
+};
+console.log(pessoa);
+arrayPessoas.push(pessoa);
+var pessoaJson = JSON.stringify(arrayPessoas);
+localStorage.setItem("pessoas", pessoaJson);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// script para o form//
-document.querySelector("form").addEventListener("submit", event =>{
-    console.log("enviar formulário")
-} )
+alert("Salvo com Sucesso");
