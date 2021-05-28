b90b816e0e1648450894452745bead59a1e92ee4
VitoriaAkemi <rm88077@fiap.com.br>
2021-05-28 15:42:57 -0300

    fale conosco js

--- js/script.js
+++ js/script.js
@@ -1,76 +1,78 @@
-//scrpt para verificar tds os seletores//
-/*const fields = document.querySelectorAll("[input]")
-
-console.log(fields)
-
-function customValidation(event) {
-    const field = event.target
-
-    function verifyErrors() {
-        let foundError = false;
-        for (let error in field.validity) {
-            if (error != "customError" && field.validity[error]) {
-                foundError = error
-            }
-        }
-        return foundError;
+function validaForm(frm) {
+    //o campo e-mail precisa de conter: "@", "." e não pode estar vazio
+    if (frm.email.value.indexOf("@") == -1 ||
+        frm.email.valueOf.indexOf(".") == -1 ||
+        frm.email.value == "" ||
+        frm.email.value == null) {
+        alert("Por favor, indique um e-mail válido.");
+        frm.email.focus();
+        return false;
     }
-    const error = verifyErrors()
-    console.log("Error Existem ", error)
+    ValidaForm(frm)
+}
 
-    if (erro) { //troca a msg no required//
-        field.setCustomValidity("Esse campo é obrigatório")
-    } else {
-        field.setCustomValidity("")
-    }
+//função de verificar se o email e senha informados são validos//
+let email = ["marcos@fiap.com.br", "marcosvinicius@fiap.com.br", "vitoria@fiap.com", "jonathan@fiap.com", "luan@fiap.com"]
+let senha = ["rm88267", "rm86904", "rm8077", "rm88082", "rm89352"]
 
-    for (let field of fields) {
-        field.addEventListener("invalid", customValidation)
-    }
+let button = document.getElementById("idLogin")
+button.addEventListener("click",login)
 
-/*login*/
-var usuarios = [
-    "batata@gmail.com", "nuvem@gmail.com" , "cores@gmail.com"
-];
-var senhas= [
-    "123456" , "abacaxi" , "654321"
-];
-
-function validar() {
-    var usuario = document.getElementById("idEmail").value;
-    var senha = document.getElementById("idPass").value;
-
-    for (var i in usuarios) {
-        var us = usuarios[i];
-        if (usuarios[i] == usuario && senhas[i] == senha) {
-            return true;
+function login(e) {
+    e.preventDefault();
+    console.log("login")
+    let txtemail = document.getElementById("email")
+    let txtidPass = document.getElementById("idPass")
+    for (i = 0; i < 5; i++) {
+        if (txtemail.value == email[i] && txtidPass.value == senha[i]) {
+            window.location.href = "../portal.html"
+            return
         }
     }
-    alert("Dados incorretos, tente novamente.");
-    return false;
+    alert("login invalido")
 }
-/*fim login*/
 
-var arrayPessoas = []
-if (JSON.parse(localStorage.getItem("pessoas")) != null) {
-    arrayPessoas.push(JSON.parse(localStorage.getItem("pessoas")));
+//função para o cadastro//
+let button1 = document.getElementById("idCadastro")
+button1.addEventListener("click", cadastro)
+
+
+let nomes = []
+
+function cadastro(e) {
+    e.preventDefault();
+    console.log("cadastrado")
+
+
+    let nome = document.getElementsById("idUser").value;
+
+    let Email = document.getElementsById("idEmail").value;
+
+    let password = document.getElementsById("idPass").value;
+
+    nomes.appendchild(nome)
+
+    email.appendchild(Email);
+
+    senha.appendchild(password);
+
+    alert("Parabens " + nomes + "seu cadastro foi concluído com sucesso")
+    window.location.href = "../portal.html"
 }
-var nome = document.getElementById("inputNome").value;
-var cpf = document.getElementById("inputCPF").value;
-var idade = document.getElementById("inputIdade").value;
-var telefone = document.getElementById("inputTelefone").value;
-var email = document.getElementById("inputEmail").value;
-var senha = document.getElementById("inputSenha").value;
 
-var pessoa = {
-    nome: nome,
-    cpf: cpf,
-    idade: idade,
-    telefone: telefone,
-    email: email,
-    senha: senha
-};
-console.log(pessoa);
-arrayPessoas.push(pessoa);
-var pessoaJson = JSON.stringify(arrayPessoas);
-localStorage.setItem("pessoas", pessoaJson);
+// função fale conosco//
+function validardados(){
+    var assunto = document.getElementById("idAssunto").value;
+    var nome = document.getElementById("idUserf").value;
+    var email = document.getElementById("idEmailf").value;
+    var comentario = document.getElementById("idComentario").value;
+
+    if (assunto == "" || nome == "" || email == "" || comentario == ""){
+       alert("Preencha todos os campos")
+       return false
+    }
+    
+    alert(
+        "Todos os campos foram preenchidos")
+        window.location.href = "../portal.html"
+}
