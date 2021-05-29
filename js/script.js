08718574a6675512734cf1dbcb108f61c073d19e
Marcos26-tech <rm88267@fiap.com.br>
2021-05-29 17:01:22 -0300

    Finalizando

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
-
-
-
-
-
 //função de verificar se o email e senha informados são validos//
+let nomes = ["Jonathan Felix ", "Marcos Maciel ", "Marcos Vinicius ", "Luan Oliveira ", "Vitoria Akemi"]
 let email = ["marcos@fiap.com.br", "marcosvinicius@fiap.com.br", "vitoria@fiap.com", "jonathan@fiap.com", "luan@fiap.com"];
 let senha = ["rm88267", "rm86904", "rm88077", "rm88082", "rm89352"];
 
@@ -25,38 +21,42 @@ function login(e) {
 }
 
 //função para o cadastro com onclick//
-function cadastro(){
-    let nome = document.getElementsById("idUser").value;
-    let Email = document.getElementsById("idEmail").value;
-    let password = document.getElementsById("idPass").value;
-    nomes.appendchild(nome)
-    email.appendchild(Email);
-    senha.appendchild(password);
-    alert("Parabens " + nomes + "seu cadastro foi concluído com sucesso");
-    window.location.href = "../portal.html";
-}
-
-
-
-
-// função fale conosco com onclick //
-function validardados() {
-    var assunto = document.getElementById("idAssunto").value;
-    var nome = document.getElementById("idUserf").value;
-    var email = document.getElementById("idEmailf").value;
-    var comentario = document.getElementById("idComentario").value;
-
-    if (assunto == "" || nome == "" || email == "" || comentario == "") {
-        alert("Preencha todos os campos");
-        return false;
-    }
-
-    alert(
-        "Todos os campos foram preenchidos");
+function cadastro() {
+    let nome = document.getElementById("idUser").value;
+    let Email = document.getElementById("idEmail").value;
+    let password = document.getElementById("idPass").value;
+    nomes.push(nome);
+    email.push(Email);
+    senha.push(password);
+    alert("Parabens " + nome + " seu cadastro foi concluído com sucesso" + "\n" + "Cadastrados: " + nomes);
     window.location.href = "./portal.html";
 }
 
 
+// função fale conosco com onclick //
+function validardados() {
+    let assunto = document.getElementById("idAssunto").value;
+    let nome = document.getElementById("idUserf").value;
+    let email = document.getElementById("idEmailf").value;
+    let comentario = document.getElementById("idComentario").value;
+
+    if (assunto == "" || nome == "" || email == "" || comentario == "") {
+        alert("Preencha todos os campos!");
+        return false;
+
+    } else {
+        alert("Todos os campos foram preenchidos, essa página ainda não envia e-mail, mas caso enviasse já estaria garantido que os campos foram preenchidos");
+        window.location.href = "./portal.html";
+
+
+    }
+
+}
+
 /**
- * Fazer aqui o comentario para o  professor
+ * Integrantes do grupo Nomes: Marcos Vinicus, Vitória Akemi, Jonathan Felix, Marcos Maciel, Luan
+ * Querido Professor Alexandre, achamos uma oportunidade de aprender a desenvolver nossas habilidades em html, css e js. 
+ * Porém em js ficou um pouco abstrato como fazer algumas coisas, esperamos que tenhamos conseguido atingir o propósito do trabalho. 
+ * Desejamos nos encontrar no próximo semestre para os próximos cps!
+ * Atenciosamente Grupo Panaceia.
  */
