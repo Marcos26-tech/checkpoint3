27164ad2f505870421f3370bd80f4c452071463f
Marcos26-tech <rm88267@fiap.com.br>
2021-05-28 22:18:49 -0300

    ajuste tds as paginas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 
+
+
+
+
 //função de verificar se o email e senha informados são validos//
 let email = ["marcos@fiap.com.br", "marcosvinicius@fiap.com.br", "vitoria@fiap.com", "jonathan@fiap.com", "luan@fiap.com"];
 let senha = ["rm88267", "rm86904", "rm88077", "rm88082", "rm89352"];
 
-let button = document.getElementById("idLogin");
-button.addEventListener("click",login);
+let input = document.getElementById("idLogin");
+input.addEventListener("click", login);
 
 function login(e) {
     e.preventDefault();
@@ -20,11 +24,8 @@ function login(e) {
     alert("login invalido");
 }
 
-//função para o cadastro//
-let button = document.getElementById("idCadastro");
-button.addEventListener("click", cadastro);
-function cadastro() {
-    console.log("cadastrado");
+//função para o cadastro com onclick//
+function cadastro(){
     let nome = document.getElementsById("idUser").value;
     let Email = document.getElementsById("idEmail").value;
     let password = document.getElementsById("idPass").value;
@@ -38,21 +39,21 @@ function cadastro() {
 
 
 
-// função fale conosco//
-function validardados(){
+// função fale conosco com onclick //
+function validardados() {
     var assunto = document.getElementById("idAssunto").value;
     var nome = document.getElementById("idUserf").value;
     var email = document.getElementById("idEmailf").value;
     var comentario = document.getElementById("idComentario").value;
 
-    if (assunto == "" || nome == "" || email == "" || comentario == ""){
-       alert("Preencha todos os campos");
-       return false;
+    if (assunto == "" || nome == "" || email == "" || comentario == "") {
+        alert("Preencha todos os campos");
+        return false;
     }
-    
+
     alert(
         "Todos os campos foram preenchidos");
-        window.location.href = "../portal.html";
+    window.location.href = "./portal.html";
 }
 
 
